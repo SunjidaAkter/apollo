55a16278d03df54451450b67a0c197c278242a4d
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-05-06 16:12:40 +0600

    generic-key-of-constraints concept implemented

--- module-3/src/generic-keyof-constraints.ts
+++ module-3/src/generic-keyof-constraints.ts
@@ -9,3 +9,9 @@ type newTypeUsingKeyOf = keyof PersonType;
 
 const a: newType = "address";
 const b: newTypeUsingKeyOf = "name";
+// ({name:'Mr. X',age:100},'age')//100
+function getProperty<X, Y extends keyof X>(obj: X, key: Y) {
+  //Y extends keyof X means Y='name'|'age'
+  obj[key];
+}
+const property = getProperty({ name: "Mr. X", age: 100 }, "age");
