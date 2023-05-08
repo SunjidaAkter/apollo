d6d000f8aeb7226f2cb321bd02c7794c0faa9240
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-05-08 08:54:49 +0600

    abstraction concept implemented

--- module-4/src/abstraction.ts
+++ module-4/src/abstraction.ts
@@ -1,3 +1,4 @@
+//Abstraction is a fundamental concept in object-oriented programming (OOP) that allows you to model complex systems in a more manageable and modular way.
 //abstraction 2 vabe kora jai
 //interface die r abstract class die
 //interface
@@ -5,16 +6,23 @@ interface Ivehicle {
   name: string;
   model: string;
 }
+const vehicle: Ivehicle = {
+  name: "Car",
+  model: "200",
+};
 interface Ivehicle1 {
   startEngine(): void;
   move(): void;
   move(): void;
 }
-const vehicle: Ivehicle = {
-  name: "Car",
-  model: "200",
-};
 class Vehicle3 implements Ivehicle1 {
+  //IVehicle holo Vehicle3 class er type...class er type prokash korte ekhane : na use kore implements use korte hoi
+  constructor(
+    public name: string,
+    public brand: string,
+    public model: number
+  ) {}
+
   startEngine(): void {
     console.log("I am starting engine");
   }
@@ -28,3 +36,33 @@ class Vehicle3 implements Ivehicle1 {
     console.log("testing");
   }
 }
+const vehicle4 = new Vehicle3("hi", "hallo", 0);
+
+//abstract class
+abstract class VehicleClass {
+  //ei class k ectends korte parbe but etar instance banano jabe na
+  constructor(
+    public name: string,
+    public brand: string,
+    public model: number
+  ) {}
+
+  abstract startEngine(): void; //abstract thakle implementation kora lage na ... etake follow kora chhara er ar kono kaj nei
+  abstract stopEngine(): void; //abstract thakle implementation kora lage na ... etake follow kora chhara er ar kono kaj nei
+  abstract move(): void; //abstract thakle implementation kora lage na ... etake follow kora chhara er ar kono kaj nei
+}
+
+class Car extends VehicleClass {
+  startEngine(): void {
+    console.log("I am starting engine...");
+  }
+  stopEngine(): void {
+    console.log("I am stopping engine...");
+  }
+  move(): void {
+    console.log("I am moving...");
+  }
+}
+
+const Car1 = new Car("car", "honda", 55);
+Car1.startEngine();
