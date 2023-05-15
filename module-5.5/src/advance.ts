7aee0b458840770b3a8f66352c9918e3bc369d3a
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-05-15 12:15:05 +0600

    advanced ts explored

--- module-5.5/src/advance.ts
+++ module-5.5/src/advance.ts
@@ -2,12 +2,28 @@ interface Model<TData, TMethod> {
   data: TData;
   methods: TMethod;
 }
-
+//properties
 interface IProperty {
   fName: string;
   lName: string;
 }
+//methods
 interface IMethod {
   fullName(): string;
 }
-// type model
+type model = Model<IProperty, IMethod>;
+
+class User implements model {
+  data: IProperty;
+  methods: IMethod;
+  constructor(fName: string, lName: string) {
+    this.data = { fName, lName };
+    this.methods = {
+      fullName() {
+        return `${fName} ${lName}`;
+      },
+    };
+  }
+}
+const user = new User("sun", "mun");
+console.log(user.methods.fullName());
